cb00e379a34ef501e3cab4d89c0fd6105464b20a
Ernesto-png <ernecampa@gmail.com>
2025-05-20 12:21:18 +0200

    aggiunto nei test un test poichè coverage al 97%, il test riguardava il troncamento che non avveniva se la stringa era troppo corta

--- test/stringUtils.test.js
+++ test/stringUtils.test.js
@@ -12,6 +12,11 @@ test('truncateString', () => {
     expect(op.truncateString('ciao', 2)).toBe('ci...');
 });
 
+test('truncateString does not truncate if string is short enough', () => {
+    expect(op.truncateString('ciao', 4)).toBe('ciao');
+    expect(op.truncateString('hi', 5)).toBe('hi');
+});
+
 test('countCharacters', () => {
     expect(op.countCharacters('ciao')).toEqual({ c: 1, i: 1, a: 1, o: 1 });
 });
